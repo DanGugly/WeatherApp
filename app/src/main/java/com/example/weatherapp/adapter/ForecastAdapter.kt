3bd8c333516d0a566a4250351510ed3984016645
DanGugly <elijahmckoy1997@hotmail.com>
2021-12-03 16:51:16 -0500

    Add ForecastAdapter.kt

---
+++ app/src/main/java/com/example/weatherapp/adapter/ForecastAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.model.Forecast
+
+class ForecastAdapter(
+    private val forecastList:MutableList<Forecast> = mutableListOf()
+) : RecyclerView.Adapter<ForecastViewHolder>(){
+
+    fun updateForecast(newForecast: List<Forecast>){
+        //Remove previous list, e.g enter atlanta, hit back, enter orlando orlando list will still have atl if we don't clear
+        forecastList.clear()
+        forecastList.addAll(newForecast)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = forecastList.size
+
+}
+class ForecastViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+
+}
