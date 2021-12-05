ffaa229ce3767111cb9d5f9ae7e2c88fc8811d56
DanGugly <elijahmckoy1997@hotmail.com>
2021-12-04 23:57:32 -0500

    Add values for onBindViewHolder and ForecastViewHolder

--- app/src/main/java/com/example/weatherapp/adapter/ForecastAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapter/ForecastAdapter.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.R
 import com.example.weatherapp.model.Forecast
@@ -28,12 +30,31 @@ class ForecastAdapter(
     }
 
     override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val forecast = forecastList[position]
+        holder.forecastCity.text = "Atlanta"
+        holder.forecastTemp.text = forecast.main.temp.toString()
+        holder.forecastMinTemp.text = forecast.main.tempMin.toString()
+        holder.forecastMaxTemp.text = forecast.main.tempMax.toString()
+        holder.forecastFeelsTemp.text = forecast.main.feelsLike.toString()
+        holder.forecastWeatherDesc.text = forecast.weather[weatherDesc].toString()
+        //holder.forecastWeatherIcon.setImageBitmap(forecast.weather[weatherIcon].toString())
     }
 
     override fun getItemCount(): Int = forecastList.size
 
+    companion object{
+        private val weatherDesc : Int = 0
+        private val weatherIcon : Int = 1
+        private val weatherId : Int = 2
+        private val weatherMain : Int = 3
+    }
 }
 class ForecastViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
-
+    val forecastCity : TextView = itemView.findViewById(R.id.forecast_city)
+    val forecastTemp : TextView = itemView.findViewById(R.id.forecast_temp)
+    val forecastMinTemp : TextView = itemView.findViewById(R.id.forecast_mintemp)
+    val forecastMaxTemp : TextView = itemView.findViewById(R.id.forecast_maxtemp)
+    val forecastFeelsTemp : TextView = itemView.findViewById(R.id.forecast_feelstemp)
+    val forecastWeatherDesc : TextView = itemView.findViewById(R.id.weather_description)
+    val forecastWeatherIcon : ImageView = itemView.findViewById(R.id.weather_icon)
 }
